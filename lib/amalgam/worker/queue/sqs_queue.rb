499d086d40d2fed5e0bce4d9f67d3a6f834922f0
Christopher Kleynhans <chris@kleynhans.ca>
2014-02-06 00:58:29 -0500

    Implemented the SqsQueue type.

--- lib/amalgam/worker/queue/sqs_queue.rb
+++ lib/amalgam/worker/queue/sqs_queue.rb
@@ -1,3 +1,14 @@
 class Amalgam::Worker::Queue::SqsQueue
+  def initialize(sqs_queue_name, idle_timeout)
+    sqs_client = AWS::SQS.new
+    @sqs_queue = sqs_client.queues.named(sqs_queue_name)
+    @idle_timeout = idle_timeout
+  end
 
+  def poll
+    message = @sqs_queue.poll(:idle_timeout => @idle_timeout)
+
+    return nil if message.nil?
+    return YAML.safe_load(message.body)
+  end
 end
